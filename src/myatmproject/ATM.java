52e694ea500e0a818a5447e19a34404a2404a950
MARIAM REDA <mrmrred8@gmail.com>
2019-03-15 21:25:40 +0200

    new commit

--- src/myatmproject/ATM.java
+++ src/myatmproject/ATM.java
@@ -6,6 +6,5 @@ public interface ATM {
 	 public void withdrawal( int amount);
 	  public void deposit (  int amount);
           //public void checkBalance( );
-/*public String prev();
-	public String next();*/
+
 }
